3db0b25ec49563b032e287e9f3e5ad0198ca2161
Bryce-Kaddouri <bryce@MyCloudPAs-MacBook-Pro.local>
2024-10-17 15:39:24 +0100

    create db fixtures

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->count(10)->create(); // Create 10 users using the factory
+    }
+}
